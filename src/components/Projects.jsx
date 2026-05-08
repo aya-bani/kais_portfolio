f1aa0e640f5825b58a2b6d712e6431c69d6d4eef
aya-bani <baniaya.it@gmail.com>
2026-05-08 16:31:59 +0100

    changingqs

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,7 @@ import {projects} from '../data' ;
 const Projects = () => {
   return (
     <div className="w-full bg-[#f2f1f0] py-16 flex flex-col items-center">
-      <h1 className="text-center mb-8 text-[#333333] text-3xl font-bold">My Projects</h1>
+      <h1 className="text-center mb-8 text-[#333333] text-3xl font-bold"> Work Showcase</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6 w-full max-w-5xl px-4">
         {projects.map((project, index) => (
           <Projects_card key={index} videoSrc={project.videoSrc} title={project.title} description={project.description} blurryimg={project.blurryimg}/>
